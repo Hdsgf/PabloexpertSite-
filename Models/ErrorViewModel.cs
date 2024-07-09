e5310fa49336b3e75a00c2f145c9faef5d24a204
Hdsgf <zubelevichivan@gmail.com>
2024-07-09 15:12:12 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PabloexpertBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
