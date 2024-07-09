6744ee615bc2cac2b3e6d8a01130dfd8810fae06
Hdsgf <zubelevichivan@gmail.com>
2024-07-10 00:14:25 +0300

    privacy delete

--- obj/Debug/net8.0/PabloexpertBlog.AssemblyInfo.cs
+++ obj/Debug/net8.0/PabloexpertBlog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PabloexpertBlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5310fa49336b3e75a00c2f145c9faef5d24a204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495600406313ae23dcb8b38791b33b23c91ffa83")]
 [assembly: System.Reflection.AssemblyProductAttribute("PabloexpertBlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PabloexpertBlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
